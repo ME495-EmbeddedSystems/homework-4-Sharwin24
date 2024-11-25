74efbd836c018227ae6703c891cdb8056d730894
Sharwin24 <sharwin24@gmail.com>
2024-11-24 23:05:36 -0600

    Starting explore node

--- nubot_nav/nubot_nav/explore.py
+++ nubot_nav/nubot_nav/explore.py
@@ -6,6 +6,12 @@ class Explore(Node):
     def __init__(self):
         super().__init__('explore')
         self.get_logger().info("Explore Node Started")
+        self.freq = 10  # [Hz]
+        self.create_timer(1.0 / self.freq, self.timer_callback)
+
+    def timer_callback(self):
+        # Run a Frontier Exploration Algorithm
+        pass
 
 
 def main(args=None):
